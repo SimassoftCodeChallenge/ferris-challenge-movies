b3ea09c9eace955acbfc3192f07005aa79bba3f3
Gabriel Simas <gabrielsimas@gmail.com>
2019-04-20 18:47:45 -0300

    Spike de Negócios 01 - Marcação dos itens que serão testados

---
+++ backend/05-Tests/Test.Challenge.Movies.Infra/Repositories/MovieRepositoryTest.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Challenge.Movies.Infra.Repositories
+{
+    [TestClass]
+    public class MovieRepositoryTest
+    {        
+        /*
+        Testes a serem efetuados:
+        1) Tela de Dashboard
+        1.1) Mostrar os seguintes serviços
+        */
+        /*
+        1.1.1) Todos os filmes Lançados nos últimos 12 meses nos EUA em pt-br
+        */
+        /*
+        1.1.2) Filmes em destaque (Trending Movies) na atualidade
+        */
+        /*
+        1.1.3) Filmes em cartaz nos cinemas do Brasil em pt-br
+        */
+        /*
+        1.1.4) Filmes com votação mais alta
+        */
+        /*
+        1.1.5) Filmes mais populares
+        1.2) Seção de Busca
+        /*
+        1.2.1) Buscar título por palavra chave em pt-br
+        */
+        /*
+        1.2.2) Mostrar os detalhes do filme que foi localizado
+        */
+        /*
+        OBS.: Cada item destes se tornará um módulo no Angular
+        */
+    }
+}
