8d84c09472fbbe689c4e01049a4b6a25a0e0d7a4
Gabriel Simas <gabrielsimas@gmail.com>
2019-04-21 19:14:54 -0300

    Spike de Negócios 01 - TDD - Camada de Infra terminada.

--- backend/05-Tests/Test.Challenge.Movies.Infra/Repositories/MovieRepositoryTest.cs
+++ backend/05-Tests/Test.Challenge.Movies.Infra/Repositories/MovieRepositoryTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.Serialization;
@@ -8,12 +9,16 @@ using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test.Challenge.Movies.Infra.Common;
+using Test.Challenge.Movies.Infra.DTO;
+using Test.Challenge.Movies.Infra.DTO.Movies;
 
 namespace Test.Challenge.Movies.Infra.Repositories
 {
     [TestClass]
     public class MovieRepositoryTest
     {
+        //FIXME: Enviar todas essas variáveis para um appSettings.json e dali extrair essas informações
         private static readonly HttpClient client = new HttpClient();
         private const string ENDPOINT_API_VERSAO4 = "https://api.themoviedb.org/4";
         private const string ENDPOINT_API_VERSAO3 = "https://api.themoviedb.org/3";
@@ -33,144 +38,141 @@ namespace Test.Challenge.Movies.Infra.Repositories
         {
             var hoje = DateTime.Now.Date;
             var anoPassado = hoje.AddMonths(-12);
-            var endpoint = $"/discover/movie?primary_release_date.gte={anoPassado.ToString("yyyy-MM-dd")}&primary_release_date.lte={hoje.ToString("yyyy-MM-dd")}&region=US&language=pt-BR&sort_by=popularity.desc";
-            //var serializer = new DataContractJsonSerializer(typeof(MovieResponseDTO));
+            var endpoint = $"/discover/movie?primary_release_date.gte={anoPassado.ToString("yyyy-MM-dd")}&primary_release_date.lte={hoje.ToString("yyyy-MM-dd")}&region=US&language=pt-BR&sort_by=popularity.desc";            
+            ResponseDTO dto = null;
 
             client.DefaultRequestHeaders.Clear();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",API_READ_ACCESS_TOKEN);
-            var resultado = await client.GetStringAsync($"{ENDPOINT_API_VERSAO4}{endpoint}");            
-            //var repositories = serializer.ReadObject(resultado) as MovieResponseDTO;
-            /*MovieResponseDTO dto = new MovieResponseDTO();
-            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(resultado)); 
-            DataContractJsonSerializer ser = new DataContractJsonSerializer(dto.GetType());  
-            dto = ser.ReadObject(ms) as MovieResponseDTO;
-            ms.Close();*/
-            var dto = Utils.SerializaToJson(resultado, new MovieResponseDTO()) as MovieResponseDTO;
+            await client.GetStringAsync($"{ENDPOINT_API_VERSAO4}{endpoint}").ContinueWith(x => {
+                dto = Utils.SerializeToJson(x.Result, new ResponseDTO()) as ResponseDTO;
+            });                                    
             Assert.AreEqual(1, dto.Page);
         }
         /*
         1.1.2) Filmes em destaque (Trending Movies) na atualidade
         */
+        [TestMethod]
+        public async Task ShouldListHighlightMovies()
+        {   
+            var endpoint = $"{ENDPOINT_API_VERSAO3}/trending/movie/day?api_key={API_KEY}";            
+            client.DefaultRequestHeaders.Clear();
+            ResponseDTO dto = null;            
+
+            await client.GetStringAsync(endpoint).ContinueWith(x => {
+                dto = Utils.SerializeToJson(x.Result, new ResponseDTO()) as ResponseDTO;
+            });
+            
+            Assert.AreEqual(1, dto.Page);            
+        }
         /*
         1.1.3) Filmes em cartaz nos cinemas do Brasil em pt-br
         */
+        [TestMethod]
+        public async Task ShouldListMoviesOnBrazilTheatres()
+        {
+            var endpoint = $"{ENDPOINT_API_VERSAO3}/movie/now_playing?api_key=bed90412e5ac5ce203465d7111fdb1f4&language=pt-BR&region=BR";            
+            ResponseDTO dto = null;            
+
+            client.DefaultRequestHeaders.Clear();
+            await client.GetStringAsync(endpoint).ContinueWith(x => {
+                dto = Utils.SerializeToJson(x.Result, new ResponseDTO()) as ResponseDTO;
+            });
+            Assert.AreEqual(1, dto.Page);            
+        }
+
         /*
         1.1.4) Filmes com votação mais alta
         */
+        [TestMethod]
+        public async Task ShouldListMoviesWithVotationHigher()
+        {
+            //var endpoint = $"{ENDPOINT_API_VERSAO4}/discover/movie?primary_release_date.gte=2018-04-15&primary_release_date.lte=2019-04-15&region=US&language=pt-BR&sort_by=vote_average.desc";
+            var hoje = DateTime.Now.Date;
+            var anoPassado = hoje.AddMonths(-12);
+            var endpoint = $"{ENDPOINT_API_VERSAO4}/discover/movie?primary_release_date.gte={anoPassado.ToString("yyyy-MM-dd")}&primary_release_date.lte={hoje.ToString("yyyy-MM-dd")}&region=US&language=pt-BR&sort_by=vote_average.desc";
+            ResponseDTO dto = null;
+
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",API_READ_ACCESS_TOKEN);
+            //var resultado = await client.GetStringAsync(endpoint);
+            await client.GetStringAsync(endpoint).ContinueWith(x => {
+                dto = Utils.SerializeToJson(x.Result, new ResponseDTO()) as ResponseDTO;
+            });
+            //dto = Utils.SerializeToJson(resultado, new ResponseDTO()) as ResponseDTO;
+            Assert.AreEqual(1,dto.Page);
+        }
         /*
-        1.1.5) Filmes mais populares
+        1.1.5) Filmes mais Populares
+        */
+        [TestMethod]
+        public async Task ShouldListMostPopularMovie()
+        {
+            var hoje = DateTime.Now.Date;
+            var anoPassado = hoje.AddMonths(-12);
+            var endpoint = $"{ENDPOINT_API_VERSAO4}/discover/movie?primary_release_date.gte={anoPassado.ToString("yyyy-MM-dd")}&primary_release_date.lte={hoje.ToString("yyyy-MM-dd")}&region=US&language=pt-BR&sort_by=popularity.desc";
+            ResponseDTO dto = null;
+
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",API_READ_ACCESS_TOKEN);
+            //var resultado = await client.GetStringAsync(endpoint);
+            await client.GetStringAsync(endpoint).ContinueWith(x => {
+                dto = Utils.SerializeToJson(x.Result, new ResponseDTO()) as ResponseDTO;
+            });
+            //dto = Utils.SerializeToJson(resultado, new ResponseDTO()) as ResponseDTO;
+            Assert.AreEqual(1,dto.Page);
+        }
+        /*
         1.2) Seção de Busca
         /*
         1.2.1) Buscar título por palavra chave em pt-br
         */
+        [TestMethod]
+        public async Task ShouldListAllMoviesSearchedbyKeyword()
+        {
+            var searchWords = "Homem Aranha";
+            var keywords = searchWords.Replace(' ','+');
+            var endpoint = $"{ENDPOINT_API_VERSAO4}/search/movie?query={keywords}&include_adult=false&language=pt-BR";
+            ResponseDTO dto = null;
+
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",API_READ_ACCESS_TOKEN);
+            //var resultado = await client.GetStringAsync(endpoint);
+            await client.GetStringAsync(endpoint).ContinueWith(x => {
+                dto = Utils.SerializeToJson(x.Result, new ResponseDTO()) as ResponseDTO;                                
+            });
+
+            if (dto.results.Count > 1)
+            {
+                var filmesOrdenadosPorDataDeLancamento = dto.results.Where(x => DateTime.Parse(x.ReleaseDate) >= DateTime.Now.Date.AddMonths(-12) && DateTime.Parse(x.ReleaseDate) <= DateTime.Now.Date).OrderByDescending(x => DateTime.Parse(x.ReleaseDate)).ToList<MovieDTO>();                
+                if (filmesOrdenadosPorDataDeLancamento != null && filmesOrdenadosPorDataDeLancamento.Count > 0){
+                    dto.results.Clear();
+                    dto.TotalResult = filmesOrdenadosPorDataDeLancamento.Count;
+                    dto.TotalPages = Math.Abs(filmesOrdenadosPorDataDeLancamento.Count / 20) + 1;
+                    dto.results.AddRange(filmesOrdenadosPorDataDeLancamento);
+                }    
+            }            
+
+            Assert.AreEqual(1,dto.Page);
+        }
         /*
         1.2.2) Mostrar os detalhes do filme que foi localizado
         */
+        [TestMethod]
+        public async Task ShouldShowMovieDetailsById()
+        {
+            int id = 324857;
+            var endpoint = $"{ENDPOINT_API_VERSAO3}/movie/{id}?api_key={API_KEY}";
+            DetailDTO dto = null;
+
+            client.DefaultRequestHeaders.Clear();
+            await client.GetStringAsync(endpoint).ContinueWith(x => {
+                dto = Utils.SerializeToJson(x.Result, new DetailDTO()) as DetailDTO;
+            });
+            Assert.AreEqual(324857, dto.Id);
+        }
         /*
         OBS.: Cada item destes se tornará um módulo no Angular
         */
+
     }
-
-    [DataContract]
-    public class MovieResponseDTO{
-        private int? _nextPage;
-        private int? _priorPage; 
-        private int? _firstPage;
-        private int? _lastPage;
-        private int? _resultsPerPage;       
-        private int _page;
-        private int _totalPages;
-
-        [DataMember(Name="page")]        
-        public int Page { 
-            get {
-                if (_page > 0)
-                {
-                    _priorPage = _page > 1 ? _page - 1 : 0;
-                    _nextPage = _page < _totalPages ? _page + 1: 0; 
-                    _lastPage = _totalPages > 0 ? _totalPages : 0;
-                    _firstPage = 1;
-                }            
-                return _page;
-            }
-
-            set { _page = value;} }
-
-        [DataMember(Name="total_results")]
-        public int TotalResult {get; set;}
-
-        [DataMember(Name="total_pages")]    
-        public int TotalPages
-        {
-            get => _totalPages;
-            set => _totalPages = value;
-        }
-        public int? PriorPage { get => _priorPage; }
-        public int? NextPage  { get => _nextPage; }
-        public int? FirstPage { get => _firstPage; }
-        public int? LastPage  { get => _lastPage; }
-        public int? ResultsPerPage => TotalResult / TotalPages;
-
-        [DataMember(Name="results")]
-        public List<MovieDTO> results;
-    }
-
-    [DataContract]
-    public class MovieDTO
-    {   
-        [DataMember(Name="poster_path")]
-        public string PosterPath { get; set; }
-
-        [DataMember(Name="adult")]
-        public bool IsAdultContent { get; set; } 
-
-        [DataMember(Name="overview")]
-        public string Overview { get; set; }
-
-        [DataMember(Name="release_date")]
-        public string ReleaseDate { get; set; }
-
-        [DataMember(Name="id")]
-        public int Id { get; set; }
-
-        [DataMember(Name="original_title")]
-        public string OriginalTitle { get; set; }
-
-        [DataMember(Name="original_language")]
-        public string OriginalLanguage { get; set; }
-
-        [DataMember(Name="title")]
-        public string Title { get; set; }
-
-        [DataMember(Name="backdrop_path")]
-        public string BackdropPath { get; set; }
-
-        [DataMember(Name="popularity")]
-        public decimal Popularity { get; set; }
-
-        [DataMember(Name="vote_count")]
-        public int VoteCount { get; set; }
-
-        [DataMember(Name="video")]
-        public bool HasVideo { get; set; }
-
-        [DataMember(Name="vote_average")]
-        public decimal VoteAverage {get; set;}
-
-        [DataMember(Name="genre_ids")]
-        public int[] genres { get; set; }
-    }
-
-    public static class Utils
-    {
-        public static object SerializaToJson(string json, object objectInput)
-        {
-            var dto = Activator.CreateInstance(objectInput.GetType());
-            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json)); 
-            DataContractJsonSerializer ser = new DataContractJsonSerializer(dto.GetType());  
-            dto = ser.ReadObject(ms);
-            ms.Close();
-            return dto;
-        }
-    }    
 }
